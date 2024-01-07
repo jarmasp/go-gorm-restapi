3bf70e548fb6403c55cdacf4c323bfe8da90be82
“Jose <armasjose606@gmail.com>
2024-01-07 01:51:26 -0400

    Feat: tasks CRUD

--- main.go
+++ main.go
@@ -15,9 +15,15 @@ func main() {
 	db.DB.AutoMigrate(models.User{})
 	r := mux.NewRouter()
 	r.HandleFunc("/", routes.HomeHandler)
+	// user routes
 	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
 	r.HandleFunc("/user/{id}", routes.GetUserHandler).Methods("GET")
 	r.HandleFunc("/user", routes.PostUsersHandler).Methods("POST")
 	r.HandleFunc("/user/{id}", routes.DeleteUsersHandler).Methods("DELETE")
+	// task routes
+	r.HandleFunc("/users", routes.GetTaskHandler).Methods("GET")
+	r.HandleFunc("/user/{id}", routes.GetTasksHandler).Methods("GET")
+	r.HandleFunc("/user", routes.PostTaskHandler).Methods("POST")
+	r.HandleFunc("/user/{id}", routes.DeleteTaskHandler).Methods("DELETE")
 	http.ListenAndServe(":3000", r)
 }
