1b9a6bbe83ee545f24d5101c3c32e3c6465a8ad2
“Jose <armasjose606@gmail.com>
2024-01-07 02:03:42 -0400

    Relate: task and user tables

--- models/task.go
+++ models/task.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Task struct {
 	gorm.Model
 	Title       string `gorm:"type:varchar(120);not null"`
-	Description string
-	Done        bool `gorm:"default:false"`
-	UserId      uint
+	Description string `json:"description"`
+	Done        bool   `gorm:"default:false"`
+	UserId      uint   `json:"UserID"`
 }
